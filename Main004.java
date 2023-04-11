162d818690d6fe7666fb9e33d98100b6019d098b
Елизавета Синявская <am8nl1492@gmail.com>
2023-04-11 15:07:10 +0300

    .\Main004.java

---
+++ Main004.java
@@ -0,0 +1,39 @@
+public class Main004 {
+    public static void main(String[] args) {
+
+        // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который
+        // вернёет «перевёрнутый» список.
+        // Ex001 ex001 = new Ex001();
+        // ex001.ReverseLinkedList();
+
+        // Реализуйте очередь с помощью LinkedList со следующими методами:
+        // • enqueue() — помещает элемент в конец очереди,
+        // • dequeue() — возвращает первый элемент из очереди и удаляет его,
+        // • first() — возвращает первый элемент из очереди, не удаляя.
+        // Ex002 ex002 = new Ex002();
+
+        // ex002.enqueue("qq");
+        // ex002.enqueue("ww");
+        // ex002.enqueue("ee");
+        // ex002.enqueue("rr");
+        // ex002.enqueue("tt");
+        // ex002.enqueue("yy");
+        // ex002.print();
+
+        // ex002.dequeue();
+        // ex002.print();
+
+        // ex002.dequeue();
+        // ex002.dequeue();
+        // ex002.print();
+
+        // ex002.first();
+        // ex002.print();
+
+        // Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке.
+        // Используйте итератор
+        Ex003 ex003 = new Ex003();
+        ex003.SummLinkedList();
+
+    }
+}
